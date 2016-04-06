6eacbd3594c4943f6d49ae33daece03c7e3ddb9e
prateek747 <prateek.mahesh747@gmail.com>
2016-04-06 20:00:25 +0530

    Added files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+
+public class Addition {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int inum = 20;
+        double fnum = (float) 3.5;
+        System.out.println(inum + fnum);
+	}
+
+}
